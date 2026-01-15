b08fd4872971309a43c73142b095d50ca00611d9
Riaru Moda <riarumoda@gmail.com>
2026-01-15 13:23:51 +0800

    script: move back to xx's hooks v1.6 for sukisu

--- build.sh
+++ build.sh
@@ -34,7 +34,7 @@ setup_environment() {
     if [[ "$KERNELSU_SELECTOR" == "--ksu=KSU_ZAKO" ]]; then
         export KSU_SETUP_URI="https://github.com/SukiSU-Ultra/SukiSU-Ultra"
         export KSU_BRANCH="builtin"
-        export KSU_GENERAL_PATCH="https://github.com/SukiSU-Ultra/SukiSU_patch/raw/refs/heads/main/hooks/scope_min_manual_hooks_v1.6.patch"
+        export KSU_GENERAL_PATCH="https://github.com/ximi-mojito-test/mojito_krenol/commit/8e25004fdc74d9bf6d902d02e402620c17c692df.patch"
     elif [[ "$KERNELSU_SELECTOR" == "--ksu=KSU_BLXX" ]]; then
         export KSU_SETUP_URI="https://github.com/backslashxx/KernelSU"
         export KSU_BRANCH="master"
